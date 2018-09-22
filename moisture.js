2bec891ecfad8e70107241c878239650f220c158
Tim Guenther <timguenther@gmail.com>
2018-09-22 12:28:22 -0700

    View min and max values over time on one sensor

--- moisture.js
+++ moisture.js
@@ -3,13 +3,23 @@ var mcpadc = require('mcp-spi-adc');
 var moistureSensor = mcpadc.open(5, {speedHz: 20000}, function (err) {
     if (err) throw err;
 
+    var moistureValues = [];
+
     setInterval(function () {
         moistureSensor.read(function (err, reading) {
             if (err) throw err;
-            var voltage = reading.value,
-                range   = (voltage / 0.8) * 100;    // Using 3.3V; will differ for 5V
-
-            console.log("Voltage: " + voltage.toFixed(4) + " => " + range.toFixed(1) + "%");
+            var mvoltage = reading.rawValue,
+		voltage  = mvoltage / 1000,
+                range    = (voltage / 0.8) * 100,    // Using 3.3V; will differ for 5V
+		moistMax = 0,
+		moistMin = 0,
+		moistDiff = 0;
+            moistureValues.push(voltage.toFixed(3));
+            moistMax = Math.max.apply(null, moistureValues);
+            moistMin = Math.min.apply(null, moistureValues);
+	    moistDiff = moistMax - moistMin;
+            //console.log("Voltage: " + voltage.toFixed(4) + " => " + range.toFixed(1) + "%");
+	    console.log("Current: " + voltage.toFixed(3) + "V, Min: " + moistMin + "V, Max: " + moistMax + "V => Difference: " + moistDiff.toFixed(3));
         });
     }, 1000);
 });
