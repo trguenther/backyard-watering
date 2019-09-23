15823635887efe2fba58c2dc2f4d4a4032b9961a
Tim Guenther <timguenther@gmail.com>
2019-09-22 18:53:40 -0700

    Put range calc logic in its own function

--- backyard.js
+++ backyard.js
@@ -103,9 +103,14 @@ function getDateTime() {
     return hour + ":" + min + " on " + month + "/" + day + "/" + year;
 }
 
+function getPercentageFromReading(value) {
+    var percentage = (value / 0.8) * 100;
+    return percentage;
+}
+
 function readoutToConsole() {
     var avg   = (m1 + m2 + m3 + m4 + m5) / 5; // Average of all five sensors
-    var range = (avg / 0.8) * 100;
+    var range = getPercentageFromReading(avg);
     var curTime = getDateTime();
 
     console.log("At " + curTime + ":\r\nAmbient temperature: " + t1 + " deg F\nAverage soil moisture reading: " + avg.toFixed(3) + "V (" + range.toFixed(1) + "%)\nSensor 1: " + m1.toFixed(3) + "V\nSensor 2: " + m2.toFixed(3) + "V\nSensor 3: " + m3.toFixed(3) + "V\nSensor 4: " + m4.toFixed(3) + "V\nSensor 5: " + m5.toFixed(3) + "V\r\n");
